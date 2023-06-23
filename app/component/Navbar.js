7c1f788e1e32c182715418bc157e29dfb58ce5a6
aziz-20 <max1990man@gmail.com.noreply.github.com>
2023-06-22 20:05:11 -0700

    modified:   app/component/Navbar.js

--- app/component/Navbar.js
+++ app/component/Navbar.js
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import {FaLaptop} from "react-icons/fa"
+import {FaLaptop,FaInstagram} from "react-icons/fa"
 export default function Navbar() {
     return (
         <nav className="bg-slate-600 p-4 sticky top-0 drop-shadow-xl z-10">
@@ -8,8 +8,8 @@ export default function Navbar() {
                 <Link className="text-white/90 no-underline hover:text-white" href="/">ALI</Link>
                 </h1>
                 <div className="flex flex-row justify-center sm:justify-evenly algin-middle gap-4 text-white text-4xl lg:text-5xl">
-                    <Link href="/projects" className="text-white/90 hover:text-white">
-                        <FaLaptop/>
+                    <Link href="https://www.instagram.com/" className="text-white/90 hover:text-white">
+                        <FaInstagram />
                     </Link>
                 </div>
             </div>
