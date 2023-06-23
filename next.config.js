78f641ff70f845506a38fcb239d058fdc7476f74
aziz-20 <max1990man@gmail.com.noreply.github.com>
2023-06-22 21:35:46 -0700

    modified:   next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+const isProd = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  // ...other configuration options...
+
+  basePath: isProd ? '/myblog' : '',
+  assetPrefix: isProd ? '/myblog/' : '',
+};
 module.exports = nextConfig
