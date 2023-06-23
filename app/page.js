086471e84ed766d273f9610ce22a39c48767653d
aziz-20 <max1990man@gmail.com.noreply.github.com>
2023-06-22 22:17:25 -0700

    modified:   app/page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Post from "./component/Post"
 export default function Home() {
   return (
     <main className="px-6 mx-auto">
-      <h1 className="text-6xl font-bold text-center text-gray-800">Hi <span className="text-blue-500">Ali</span></h1>
+      <h1 className="text-6xl font-bold text-center text-gray-800 my-5">Hi <span className="text-blue-500">Ali</span></h1>
       <Post />
     </main>
   )
